1d8ee0c32ac5525144d7d7401e4da25e626e7fab
Alejandro <13650diaz@gmail.com>
2022-10-24 10:59:33 -0400

    added

--- parabolic.py
+++ parabolic.py
@@ -1,11 +1,13 @@
 # parabolic equation
 import math
 
+#### CHANGE THESE VALUES
+r0 = 2 			# 2 mm (radius of quadrupole)
+height = 1.8	# put height in mm
+#### END OF CHANGING VALUES
 
-r0 = 2.25 		# 2.25 mm squared
-x = 1.73
-y = math.sqrt(x**2+r0**2)
-
+x = math.sqrt((r0+height)**2 - r0**2)	
+x_total_height =  x * 2 
 # x = math.sqrt(y**2-r0**2)
-print("x: ", x)
-print("y: ", y)
+print("x: ", x_total_height, "mm")
+print("height: ", height, "mm")
--- starterLuis.py
+++ starterLuis.py
@@ -17,16 +17,18 @@ h = 15 						# constant dependent on stability region- 1st: 10-20, 2nd:.73-1.43
 # q = 3.23
 # h = 1 						# constant dependent on stability region- 1st: 10-20, 2nd:.73-1.43
 
+
+# in kilograms
 # m = 4.981* 10**-25		# 300 amu
-m = 1.66054 * 10**-25		# 100 amu
-# m = 6.64216*10**-26		# 40amu
-# m = 1.82659 * 10**-27		# 1.1 amu
-# m = 1.66054* 10**-27		# 1 amu
+m_100 = 1.66054 * 10**-25		# 100 amu
+m_argon40 = 6.64216*10**-26		# 39.948amu argon
+m_carbon12 = 1.99447*10**-26	# 12.011 amu carbon
+m_1_1 = 1.82659 * 10**-27		# 1.1 amu
+m_hydrogen1 = 1.66054* 10**-27		# 1.00784 amu
 
 
-ez = 5 * 1.60218*10**-19	# making assumption of 5 electron volts & convert to joules
-e = 1.60217662*10**-19
-vz = 2*(ez/m)**(1/2)		# axial ion velocity
+
+
 resolutions = list()
 voltages = list()
 frequencies = list()
@@ -34,7 +36,13 @@ lengths = list()
 
 
 # in Hz and meters
-def evaluate(frequency, L, r0, m, e, ez, vz, h, q, a):
+def evaluate(frequency, L, r0, m, h, q, a):
+
+	ez = 5 * 1.60218*10**-19	# making assumption of 5 electron volts & convert to joules
+	e = 1.60217662*10**-19
+	vz = 2*(ez/m)**(1/2)		# axial ion velocity
+
+
 	resolution = 1/h*(frequency*(L/vz))**2
 	print("resolution: ", resolution)
 	# delta_m = 2*h*ez/(frequency**2*L**2)
@@ -79,12 +87,27 @@ def evaluate(frequency, L, r0, m, e, ez, vz, h, q, a):
 	# plt.show()
 
 print()
-evaluate(frequency= 2650000, L = .1, r0 = .002, m = m, e=e, ez=ez, vz=vz, h=h, q=q, a=a) # original
-evaluate(frequency= 2650000, L = .1, r0 = .00175, m = m, e=e, ez=ez, vz=vz, h=h, q=q, a=a) # radius -s equal to 2.25mm
+# evaluate(frequency= 2650000, L = .1, r0 = .002, m = m, e=e, ez=ez, vz=vz, h=h, q=q, a=a) # original
+# evaluate(frequency= 2650000, L = .1, r0 = .00175, m = m, e=e, ez=ez, vz=vz, h=h, q=q, a=a) # radius -s equal to 2.25mm
 # evaluate(frequency= 2650000, L = .1, r0 = .002, m = m, e=e, ez=ez, vz=vz, h=h, q=q, a=a) # radius -s equal to 2.25mm
 # evaluate(frequency= 2650000, L = .1, r0 = .002, m = m, e=e, ez=ez, vz=vz, h=h, q=q, a=a) # radius -s equal to 2.25mm
 # evaluate(frequency= 1440000, L = .08, r0 = .002, m = m, e=e, ez=ez, vz=vz, h=h, q=q, a=a)
+# evaluate(frequency= 930000, L = .0484, r0 = .002, m = m, e=e, ez=ez, vz=vz, h=h, q=q, a=a) # original
 
+# evaluate(frequency= 930000, L = .0484, r0 = .002, m = 1.14577*10**-25, e=e, ez=ez, vz=vz, h=h, q=q, a=a) # fc-43 69 amu
+# evaluate(frequency= 930000, L = .0484, r0 = .002, m = 2.1753*10**-25, e=e, ez=ez, vz=vz, h=h, q=q, a=a) # fc-43 131 amu
+# evaluate(frequency= 930000, L = .0484, r0 = .002, m = 3.6366*10**-25,  e=e, ez=ez, vz=vz, h=h, q=q, a=a) # fc-43 219 amu 
+# evaluate(frequency= 930000, L = .0484, r0 = .002, m = 4.3838*10**-25,  e=e, ez=ez, vz=vz, h=h, q=q, a=a) # fc-43 264 amu
+# evaluate(frequency= 930000, L = .0484, r0 = .002, m = 6.6421*10**-26,  e=e, ez=ez, vz=vz, h=h, q=q, a=a) # fc-43 40 amu argon
+
+# evaluate(frequency= 1780000, L = .0734, r0 = .002, m = m_hydrogen1, h=h, q=q, a=a)
+# evaluate(frequency= 1780000, L = .0734, r0 = .002, m = m_1_1, h=h, q=q, a=a)  
+# evaluate(frequency= 1780000, L = .0734, r0 = .002, m = m_carbon12, h=h, q=q, a=a)
+# evaluate(frequency= 1780000, L = .0734, r0 = .002, m = m_argon40, h=h, q=q, a=a) 
+evaluate(frequency= 2200000, L = .0734, r0 = .002, m = m_100, h=h, q=q, a=a)
+
+
+# evaluate(frequency= 2650000, L = .1, r0 = .002, m = m_100, h=h, q=q, a=a)
 
 
 
